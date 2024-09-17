0fce9474a177a98dad4622625ae262692521b5e1
Danny Finnegan <dannyfinnegan60@gmail.com>
2024-09-17 14:26:32 +0100

    Bug Fixing

--- app_server/Controllers/main.js
+++ app_server/Controllers/main.js
@@ -2,7 +2,11 @@
 const index = function(req, res){ 
     res.render('index', { title: 'Express' }); 
     };
+    const about = function(req, res){ 
+        res.render('index', { title: 'About' }); 
+        };
     module.exports = { 
     index,
+    about
     };
     
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const ctrlMain = require('../Controllers/main'); 
 /* GET home page. */
 router.get('/', ctrlMain.index); 
+router.get('/about', ctrlMain.about);
 module.exports = router;
