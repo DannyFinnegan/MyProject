c359f2c44786433bff0096c3ac78920db189ff81
Danny Finnegan <dannyfinnegan60@gmail.com>
2024-10-07 17:47:08 +0100

    For controller pushback

--- app_server/Controllers/locations.js
+++ app_server/Controllers/locations.js
@@ -1,34 +1,36 @@
 /* GET 'home' page */
 const homelist = function (req, res) {
-    res.render('data', { 
+    res.render('data', {
         title: 'Graphics Cards',
-         GraphicsCards: [{
-            name: 'Nvidia RTX 4070 Super',
-            price: '€356.99',
-            rating: 3,
-            sale: '20%'
-         },
-         {
-            name: 'Nvidia RTX 4080 Super',
-            price: '€566.99',
-            rating: 4,
-            sale: '10%'
-         },
-        {
-            name: 'Nvidia RTX 4060',
-            price: '€296.99',
-            rating: 3,
-            sale: '0%'
-        },
-        {
-            name: 'Nvidia RTX 4090',
-            price: '€756.99',
-            rating: 5,
-            sale: '35%'
-        }
-        ]
-        });
-    
+        GraphicsCards:
+            [
+                {
+                    name: 'Nvidia RTX 4070 Super',
+                    price: '€356.99',
+                    rating: 3,
+                    sale: '20%'
+                },
+                {
+                    name: 'Nvidia RTX 4080 Super',
+                    price: '€566.99',
+                    rating: 4,
+                    sale: '10%'
+                },
+                {
+                    name: 'Nvidia RTX 4060',
+                    price: '€296.99',
+                    rating: 3,
+                    sale: '0%'
+                },
+                {
+                    name: 'Nvidia RTX 4090',
+                    price: '€756.99',
+                    rating: 5,
+                    sale: '35%'
+                }
+            ]
+    });
+
 };
 /* GET 'Location info' page */
 const register = function (req, res) {
