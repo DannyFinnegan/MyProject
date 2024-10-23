7575f48db32ac3690899ca31f42475b1fb9ed7a8
danny <daniel.finnegan@students.ittralee.ie>
2024-10-23 13:29:19 +0100

    Test

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const ctrlLocations = require('controllers/locations');
-const ctrlOthers = require('controllers/others'); 
+const ctrlLocations = require('../controllers/locations'); // Updated path
+const ctrlOthers = require('../controllers/others'); // Updated path
+
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/register', ctrlLocations.register);
 router.get('/login', ctrlLocations.login);
+
 /* Other pages */
 router.get('/data', ctrlOthers.data);
+
 module.exports = router;
