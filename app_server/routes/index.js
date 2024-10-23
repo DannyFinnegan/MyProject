4c342d1105bcd6a0cc011a0a18f0d25147f57108
danny <daniel.finnegan@students.ittralee.ie>
2024-10-23 13:26:37 +0100

    Refactor import paths for controllers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const ctrlLocations = require('../controllers/locations');
-const ctrlOthers = require('../controllers/others'); 
+const ctrlLocations = require('controllers/locations');
+const ctrlOthers = require('controllers/others'); 
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/register', ctrlLocations.register);
