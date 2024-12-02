757dac863038e9abcb9c0266075e1bf697e31c74
Danny Finnegan <dannyfinnegan60@gmail.com>
2024-12-02 11:57:18 +0000

    Add login and registration routes with corresponding views and update layout for navigation

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,6 +3,21 @@ const router = express.Router();
 const ctrlProducts = require('../controllers/data');
 
 router
+    .route('/login')
+    .get(ctrlProducts.LoadLogin)
+    //.post(ctrlProducts.CreateUser);
+
+router
+    .route('/register')
+    .get(ctrlProducts.LoadRegister);
+
+router
+    .route('/user/:Email')
+    .get(ctrlProducts.UserLogin);
+
+router.get('/favicon.ico', (req, res) => res.status(204));
+
+router
     .route('/')
     .get(ctrlProducts.DataGatherProducts)
     .post(ctrlProducts.DataCreate);
@@ -10,15 +25,11 @@ router
 router
     .route('/:productid')
     .get(ctrlProducts.ProductsReadOne)
-    .put(ctrlProducts.ProductsUpdateOne) 
+    .put(ctrlProducts.ProductsUpdateOne)
     .delete(ctrlProducts.ProductsDeleteOne);
 
-router
-    .route('/user')
-    .post(ctrlProducts.CreateUser);
-
-router
-    .route('/user/:Email')
-    .get(ctrlProducts.UserLogin);
+router.use((req, res, next) => {
+    res.status(404).json({ message: 'Route not found' });
+});
 
 module.exports = router;
